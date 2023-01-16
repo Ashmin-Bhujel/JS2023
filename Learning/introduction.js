30f24fa26fb52decebae677d415bb36f5594f3be
ashmin-bhujel <ashminbhujel01@gmail.com>
2023-01-16 16:32:23 +0545

    Added Day 1 files

---
+++ Learning/introduction.js
@@ -0,0 +1,39 @@
+// Introduction to JavaScript
+
+// JavaScript is a general purpose dynamically typed programming language.
+// One of the popular programming language used for web development, cross-platform mobile app development.
+// Built to add interactivity to web pages in 1990's (around 1995).
+// Standardized by ECMA and is also know as ECMAScript.
+// Some popular JavaScript engines are V8 (Chrome, Node.js), Chakra (Microsoft), SpiderMonkey (Mozilla Firefox).
+// ES6: ES2015 is the start of the Modern JavaScript.
+
+// Use strict mode to prevent mistakes
+"use strict";
+
+// Printing "Hello, JavaScript!" on the console
+console.log("Hello, JavaScript!");
+
+// Variables
+var programmingLanguage = "Mocha";
+console.log(programmingLanguage);
+
+// Changing from "Mocha" to "LiveScript"
+programmingLanguage = "LiveScript";
+console.log(programmingLanguage);
+
+// Java was popular in 1990's
+// Changing from "LiveScript" to "JavaScript"
+programmingLanguage = "JavaScript";
+console.log(programmingLanguage);
+
+// Variables using "let" keyword
+let year = 2023;
+// Cannot declare again using let
+// let year = 2023;
+console.log(`year: ${year}`);
+
+// Constants
+const PI = 3.1415;
+// Cannot change value of a constant
+// PI = 3.14;
+console.log(`Value of PI: ${PI}`);
